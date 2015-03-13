21db02289a4af47d0ce3c48853ee5a9d91593129
devkont <yusupovb@gmail.com>
2015-03-13 12:22:53 +0300

    Список запрещенных логинов

--- logincheck/logincheck.users.register.add.validate.php
+++ logincheck/logincheck.users.register.add.validate.php
@@ -8,6 +8,13 @@ Order=1
 
 defined('COT_CODE') or die('Wrong URL');
 
-if (!empty($ruser['user_name']) && !preg_match("/^[a-zA-Z][_a-zA-Z0-9-]*$/i", $ruser['user_name'])) cot_error('aut_invalidloginchars', 'rusername');
+require_once cot_langfile('logincheck', 'plug');
 
-?>
+if (!empty($ruser['user_name']) && !preg_match("/^[a-zA-Z][_a-zA-Z0-9-]*$/i", $ruser['user_name'])) cot_error($L['logincheck_error_invalidchars'], 'rusername');
+
+if(!empty($cfg['plugin']['logincheck']['invalidnames'])){
+	$invalidnames = explode(',', $cfg['plugin']['logincheck']['invalidnames']);
+	if(is_array($invalidnames)){
+		if (!empty($ruser['user_name']) && in_array($ruser['user_name'], $invalidnames)) cot_error($L['logincheck_error_invalidname'], 'rusername');
+	}
+}
